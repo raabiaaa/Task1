e9599da17e52cf16ef6b7c88d3dd65802bfe75ac
hp-PC\hp <rabii_tariq@hotmail.com>
2018-06-13 22:28:19 +0500

    write any commit here..writing for exmple

--- HelloWorld.c
+++ HelloWorld.c
@@ -5,7 +5,7 @@ int main()
 printf("Hello, world!\n");
  printf("Name = Rabia Tariq\n");
  printf("************\n");
- printf("i made changes");
+ printf("i made changes for sourctree video");
  printf("Roll no.= BCS-F15-03\n\n");
 return (0);
 }
