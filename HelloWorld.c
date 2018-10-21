053f1d2d10331e29b443d88dce3694d36c66c90d
raabiaaa <38428088+raabiaaa@users.noreply.github.com>
2018-10-21 18:33:16 +0500

    Update  HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -6,6 +6,8 @@ printf("Hello, world!\n");
  printf("Name = Rabia Tariq\n");
  printf("************\n");
  printf("i made changes for sourctree video");
+ printf("Tabdeeli aa nahi rahi");
+ printf("Tabdeeli aa gayi hai ");
  printf("Roll no.= BCS-F15-03\n\n");
 return (0);
 }
